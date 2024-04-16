c7470e31f2b20512e2240f59d6e22472dee085bf
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-04-15 22:23:50 -0300

    Primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimeiroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'game-launcher-app';
 }
+
+
+
