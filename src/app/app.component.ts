e9d59fd9ba5f13b907d8c5c1615efe47830b4797
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-05-05 18:02:04 -0300

    Segundo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeiroComponent],
+  imports: [RouterOutlet, PrimeiroComponent, SegundoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
