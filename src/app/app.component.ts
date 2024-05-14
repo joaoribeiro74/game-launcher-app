4252f33d590676e4c2efc4fdb499f58dd0e82f0b
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-05-14 18:41:01 -0300

    atualização tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< Updated upstream
-=======
-import { PrimeiroComponent } from './primeiro/primeiro.component';
-import { SegundoComponent } from './segundo/segundo.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [RouterOutlet],
-=======
-  imports: [RouterOutlet, PrimeiroComponent, SegundoComponent, NavbarComponent, FooterComponent],
->>>>>>> Stashed changes
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
