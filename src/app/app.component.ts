019bf35f7c7cde256f7bab6ef9911c4aad39fa5a
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-05-14 18:28:48 -0300

    gitflow-feature-stash: atividade-10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
+=======
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet],
+=======
+  imports: [RouterOutlet, PrimeiroComponent, SegundoComponent, NavbarComponent, FooterComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
