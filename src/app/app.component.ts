67086b9d67257c1cbc504c5b4f4fcd4de0e737b3
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-06-02 22:49:03 -0300

    gitflow-feature-stash: atividade-11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TwoWayDatabindingComponent } from './two-way-databinding/two-way-databinding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, TwoWayDatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
